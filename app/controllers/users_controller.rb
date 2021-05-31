73884f4382d7c8dc0e1141f6419dc94daba57153
gzim921 <gzimiseni7@gmail.com>
2021-05-31 23:32:56 +0200

    Authenticate user with JWT

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+  before_action :authorized, only: [:auto_login]
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: 'Invalid username or password'}
+    end
+  end
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: 'Invalid username or password'}
+    end
+  end
+
+  def auto_login
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.permit(:user, :email, :username, :password, :password_confirmation)
+  end
 end
