7ab59d4a4e94b13d8723d2851e5ab104c9e01653
gzim921 <gzimiseni7@gmail.com>
2021-06-05 20:18:57 +0200

    Created some additional tests when user not logged in

--- spec/request/authentication_processes_spec.rb
+++ spec/request/authentication_processes_spec.rb
@@ -1,32 +1,51 @@
-equire 'rails_helper'
+require 'rails_helper'
 
-RSpec.describe "AuthenticationProcesses" do
+RSpec.describe 'AuthenticationProcesses' do
   let(:user) { create(:user) }
-  let(:token_new) { JWT.encode({user_id: user.id}, Rails.application.secrets.secret_key_base) }
+  let(:token_new) { JWT.encode({ user_id: user.id }, Rails.application.secrets.secret_key_base) }
   let(:token) do
-    {  "Authorization": "Bearer #{token_new}"  }
+    { 'Authorization': "Bearer #{token_new}" }
   end
 
   describe 'loggin in and signing up' do
-    it 'should login successfully' do 
-      post '/api/v2/login', params: { email: user.email, password: user.password }, headers: token
+    context 'when giving the right parameters' do
+      it 'should login successfully' do
+        post '/api/v2/login', params: { email: user.email, password: user.password }
 
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include(user.email)
-      expect(response.body).to include(token_new)
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to include(user.email)
+        expect(response.body).to include(token_new)
+      end
+
+      it 'should register successfully' do
+        post '/api/v2/users', params: { user: { username: 'test222',
+                                                email: 'test222@gmail.com',
+                                                password: 'test',
+                                                password_confirmation: 'test' } }
+
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    it 'should register successfully' do
-      post '/api/v2/users', params: { user: { username: "test222",
-                                            email: "test222@gmail.com",
-                                            password: "test",
-                                            password_confirmation: "test" } }
+    context 'when passing wrong parameters' do
+      it 'should not login' do
+        post '/api/v2/login', params: { email: '', password: '' }
 
-      expect(response).to have_http_status(:ok)
+        expect(response.body).to be_json.with_content({ error: 'Invalid email or password!' })
+      end
+
+      it 'should not be able to register' do
+        post '/api/v2/users', params: { user: { username: '',
+                                                email: '',
+                                                password: '',
+                                                password_confirmation: '' } }
+
+        expect(response.body).to be_json.with_content({ error: 'Invalid inputs!' })
+      end
     end
   end
 
-  describe 'requesting flowers and sightings flower without logging in' do
+  describe 'requesting flowers and sightings flower as logged in or not logged in' do
     let!(:flower) { create(:flower) }
     let!(:sighting) { create(:sighting) }
 
@@ -35,39 +54,31 @@ RSpec.describe "AuthenticationProcesses" do
 
       expect_json_types(flowers: :array_of_objects)
     end
-
     it 'should render flower and sightings and validate types' do
       get "/api/v2/flowers/#{flower.id}/sightings"
       create_list(:sighting, 2, flower: flower)
-
       expect(response.body).to include(flower.name)
       expect_json_types(sightings: :array_of_objects)
     end
   end
-
   describe 'as a logged in user' do
     let!(:flower) { create(:flower) }
     let!(:sighting) { create(:sighting) }
-
     it 'can create sighting' do
       post "/api/v2/flowers/#{flower.id}/sightings", params: { logitude: 12.111111,
-                                            latitude: 23.123123,
-                                            question: 'Nothing to worry about?' }, headers: token
+                                                               latitude: 23.123123,
+                                                               question: 'Nothing to worry about?' }, headers: token
 
       expect(response).to have_http_status(:ok)
       expect_json_types(sightings: :array_of_objects)
     end
-
     it 'can like a sighting' do
       post "/api/v2/sightings/#{sighting.id}/likes", headers: token
-
       expect(response.body).to be_json.with_content({ note: 'Sighting liked successfully!' })
     end
-
     context 'when destroying' do
       it 'cannot destroy others sightings' do
         delete "/api/v2/flowers/#{flower.id}/sightings/#{sighting.id}", headers: token
-
         expect(response.body).to be_json.with_content({ note: 'You dont have permission!!' })
       end
 
