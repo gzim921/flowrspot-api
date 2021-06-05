30fd2cbdb3e15af0087e3574bc528255124ee4a8
gzim921 <gzimiseni7@gmail.com>
2021-06-05 19:52:07 +0200

    Modified flower model testing

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,5 +1,5 @@
 class Flower < ApplicationRecord
   has_one_attached :image
-  validates :image, attached: false, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  validates :image, attached: false
   has_many :sightings, dependent: :destroy
 end
--- spec/model/flower_spec.rb
+++ spec/model/flower_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Flower do
-  describe 'validations' do
-    it { is_expected.to validate_content_type_of(:image).allowing('image/png', 'image/jpg', 'image/jpeg') }
-  end
-
   describe 'associations' do
     it { should have_many(:sightings) }
 
